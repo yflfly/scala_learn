e92ec71925759e333dd46b8abf5fd5dba1c258a2
yflfly <1091283542@qq.com>
2021-06-22 19:36:50 +0800

    parallelized

--- src/main/scala/com.fly.etl/ScalaLearnv1.scala
+++ src/main/scala/com.fly.etl/ScalaLearnv1.scala
@@ -5,11 +5,18 @@ import org.apache.spark.{SparkConf, SparkContext}
 object ScalaLearnv1 {
   def main(args: Array[String]): Unit = {
     // args(0) -> 1
-    // 1.设置spark配置信息
-    val conf = new SparkConf().setAppName("SparkLearn").setMaster("local")
+    // 1.设置spark配置信息 build a sparkconf object that contains information about your application
+    val conf = new SparkConf().setAppName("SparkLearn").setMaster("local") // local : to run in local mode
 
-    // 2. 设置运行信息
+    // 2. 设置运行信息 tells spark how to access a cluster
     val sc = new SparkContext(conf)
+
+    // 3. parallelized collections
+    // to create a parallelized collection holding the numbers 1 to 5
+    val data = Array(1, 2, 3, 4, 5)
+    val distData = sc.parallelize(data)
+    println(distData) // 打印结果 ParallelCollectionRDD[0] at main at <unknown>:0
+
   }
 
 }
