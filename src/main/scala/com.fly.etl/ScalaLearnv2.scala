5a9de4fdb8dd343e4a2c5aab60997561d62cb56c
yflfly <1091283542@qq.com>
2021-06-23 19:52:17 +0800

    提交

--- src/main/scala/com.fly.etl/ScalaLearnv2.scala
+++ src/main/scala/com.fly.etl/ScalaLearnv2.scala
@@ -12,13 +12,13 @@ object ScalaLearnv2 {
     val filepath = "/Users/a58/companyproject/scala_learn/src/main/resources/people.json"
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-    val people = sqlContext.jsonFile(filepath)
-    people.printSchema()
+    val people = sqlContext.jsonFile(filepath) //jsonFile：从一个包含json文件的目录中加载，文件中的每一行是一个json对象
+    people.printSchema() // 注意：一个多行的json文件经常会失败
     people.registerTempTable("people")
-//    val teenagers = sqlContext.sql("SELECT name FROM people WHERE age >= 13 AND age <= 19")
-//    val anotherPeopleRDD = sc.parallelize(
-//      """{"name":"Yin","address":{"city":"Columbus","state":"Ohio"}}""" :: Nil)
-//    val anotherPeople = sqlContext.jsonRDD(anotherPeopleRDD)
+    val teenagers = sqlContext.sql("SELECT name FROM people WHERE age >= 13 AND age <= 19")
+    val anotherPeopleRDD = sc.parallelize(
+      """{"name":"Yin","address":{"city":"Columbus","state":"Ohio"}}""" :: Nil)
+    val anotherPeople = sqlContext.jsonRDD(anotherPeopleRDD)
   }
 
 }
